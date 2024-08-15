3b1a294e510758076785d3e8e52a6741d467275c
Filipe Rabelo F Lana <filipe.rabelolana@hotmail.com>
2024-08-15 19:20:03 -0300

    jogo mario run

---
+++ js/script.js
@@ -0,0 +1,39 @@
+
+const mario = document.querySelector(".mario");
+const pipe  = document.querySelector(".pipe");
+
+const jump = () => {
+  mario.classList.add("jump");
+
+  setTimeout(() => {
+    mario.classList.remove("jump");
+  }, 500);
+};
+
+const loop = setInterval(() => {
+  const pipePosition  = pipe.offsetLeft;
+  const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '');
+
+  console.log(marioPosition);
+
+  // condicao onde o jogo acaba
+
+  if(pipePosition <= 160 && pipePosition && marioPosition < 80 ){
+
+    pipe.style.animation   = "none";
+    pipe.style.left        = `${pipePosition}px`;
+
+    mario.style.animation  = "none";
+    mario.style.bottom     = `${marioPosition}px`;
+
+    mario.src              = './img/game-over.png';
+    mario.style.width      = '75px';
+    mario.style.marginLeft = '50px';
+
+    clearInterval(loop); // para parar o loop
+
+  }
+
+}, 10);
+
+document.addEventListener("keydown", jump);
